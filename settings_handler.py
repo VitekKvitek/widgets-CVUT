b2991e31972a4b92f352a5609d60a5a2089ad174
VitekKvitek <vitekkvitek@duck.com>
2024-07-23 17:52:19 +0200

    updated lists to dictionary - saving now requires to add name

--- settings_handler.py
+++ settings_handler.py
@@ -1,30 +1,35 @@
 import json
 
-widget_list = []
+widget_dic = {}
 # Adds widget to the list of widgets which are going to be stored
-def add(new_widget):
-    widget_list.append(new_widget)
+def add(new_widget, name):
+    widget_dic[name] = new_widget
+    # widget_list.append(new_widget)
+    
 # Function to collect widget states
 def get_widget_values():
-    values_list = []
-    for widget in widget_list:
-        values_list.append(widget.value)
-    return values_list
+    values_dic = {}
+    # TODO
+    for name, widget in widget_dic.items():
+        values_dic[name] = widget.value
+    return values_dic
+
 # _throwaway_ because button sends information about itself as argument
 def save(_throwaway_):
     # Dump the list of values to file
-    values_list = get_widget_values()
+    values_dic = get_widget_values()
     with open('data/settings.json', 'w') as file:
-        json.dump(values_list, file)
+        json.dump(values_dic, file)
 
 # Function to load widget values
-def load_widget_states(loaded_value_list):
-    for index,widget in enumerate(widget_list):
-        widget.value = loaded_value_list[index]
+def load_widget_states(loaded_value_dic):
+    for name,loaded_value in loaded_value_dic.items():
+        widget_dic[name].value = loaded_value
+        
 # _throwaway_ because button sends information about itself as argument
 def load(_throwaway_):
     # Load the list of values back from the file
     with open('data/settings.json', 'r') as file:
-        loaded_vlaue_list = json.load(file)
-    load_widget_states(loaded_vlaue_list)
+        loaded_vlaue_dic = json.load(file)
+    load_widget_states(loaded_vlaue_dic)
     
