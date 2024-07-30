9e8abe3684d55f59694361a7a33617871983a21d
JiriikGA <jiri.petrik@student.gyarab.cz>
2024-07-30 17:55:22 +0200

    added slider

--- plot.py
+++ plot.py
@@ -1,12 +1,8 @@
 import cv2 as cv
 import numpy as np
+import os
 from os import listdir, path
-from os.path import isfile, isdir, join
-
-import matplotlib.pyplot as plt
-import ipywidgets as widgets
-from IPython.display import display
-from IPython.display import FileLink
+from os.path import isfile, join
 
 
 def drawOverlay(opacity, original_image, original_gt, dataset):
@@ -82,5 +78,54 @@ def get_all_files(folder_path):
 def get_all_folders(directory):
     # Return only directories, not files
     return [name for name in listdir(directory) if path.isdir(path.join(directory, name))]
+
+def contract(name):
+    match name:
+        case "Fishyscapes_LaF":
+            contraction = 'FS'
+
+        case "RoadAnomaly":
+            contraction = 'RA'
+
+        case "RoadObstacles":
+            contraction = 'RO'
+        
+        case "RoadObstacles21":
+            contraction = 'RO21A'
+        case _:
+            print("Could not find correct contraction name")
+            contraction = None
+
+    return contraction
+
+def load_images(selected_file, use_dataset, selected_folder, selected_model):
+    # Determine folder paths
+    base_folder_path = 'data/export/datasets/' + selected_folder + '/test/'
+    if use_dataset:
+        gt_folder_path = os.path.join(base_folder_path, 'gt')
+        original_gt_path = os.path.join(gt_folder_path, selected_file)
+    else:
+        results_folder = os.path.join('data/export/results/', selected_model, contract(selected_folder), 'preds')
+        print(results_folder)
+        base_filename = os.path.splitext(selected_file)[0]
+        original_gt_path = os.path.join(results_folder, base_filename+"..png.npy")
+    print(original_gt_path)
+
+    # Load ground truth
+    original_gt = cv.imread(original_gt_path) if use_dataset else np.load(original_gt_path)
+    assert original_gt is not None, f"original_gt could not be read from {original_gt_path}"
+
+    # Load original image
+    imgs_folder_path = os.path.join(base_folder_path, 'imgs')
+    original_image_path = os.path.join(imgs_folder_path, selected_file)
+    #print(original_image_path)
+    original_image = cv.imread(original_image_path)
+    assert original_image is not None, f"original_image could not be read from {original_image_path}"
+
+    # Convert BGR to RGB format
+    original_image_rgb = original_image[:, :, [2, 1, 0]]
+    return original_image_rgb, original_gt
+
+
 
 
