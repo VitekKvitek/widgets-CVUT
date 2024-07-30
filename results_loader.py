7fba683532a31c71186a536a3590d70f313ceeb7
VitekKvitek <vitekkvitek@duck.com>
2024-07-30 09:20:20 +0200

    working read data

--- results_loader.py
+++ results_loader.py
@@ -9,22 +9,15 @@ def read_json(file_path):
         value_AP = data['AP']
         value_FPRat95 = data['FPRat95']
         return value_AP, value_FPRat95
-
-
-def read_json_files(folder_path):
-    # List all files in the given folder
-    files = os.listdir(folder_path)
-    
-    # Filter out JSON files
-    json_files = [file for file in files if file.endswith('.json')]
-    
-    for json_file in json_files:
-        file_path = os.path.join(folder_path, json_file)
+# This function serves for loading results from data folder
 def read_all_jsons():
     method_list = []
     dataset_list = []
-    score_list = []
+    # 2D list of AP and FPRat95 values
+    all_score_list = []
     for method_folder in os.listdir(initial_folder):
+        # 1D list of AP and FPRat95 one just for speciffic method
+        solo_score_list = []
         method_list.append(method_folder)
         m_folder_path = os.path.join(initial_folder, method_folder)
         dataset_folders = os.listdir(m_folder_path)
@@ -33,12 +26,9 @@ def read_all_jsons():
             dataset_list.append(dataset_f)
             json_r_path = os.path.join(m_folder_path, dataset_f, json_general_path)
             value_AP, value_FPRat95 = read_json(json_r_path)
-# Example usage
-file_path = 'data/export/results/grood_logml_1000K_01adamw_tau10_resetthr1/RO21A/res/res.json'
+            solo_score_list.append(value_AP)
+            solo_score_list.append(value_FPRat95)
+        all_score_list.append(solo_score_list)
+    return method_list, dataset_list, all_score_list
 initial_folder = 'data/export/results'
 json_general_path = 'res/res.json'
-
-
-read_all_jsons()
-# TODO
-dataset = file_path.split('/')[-3]
