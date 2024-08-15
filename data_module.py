eecb54a9b2ab7f2d1986b26f76bfa2bde31169dd
JiriikGA <jiri.petrik@student.gyarab.cz>
2024-08-15 16:21:17 +0200

    added button to toggle ignore region

--- data_module.py
+++ data_module.py
@@ -36,7 +36,8 @@ image_values = {
     'selected_folder': 'Fishyscapes_LaF',
     'selected_algo': ['grood_logml_1000K_01adamw_tau10_resetthr1',
                       'grood_knn_e2e_cityscapes_500k_fl003_condensv5_randomcrop1344_hflip_nptest_lr0025wd54_ipdf0_ioodpdf0uni1_staticood1'],
-    'threshold': [0.997, 0.997]
+    'threshold': [0.997, 0.997],
+    'ignore': False
 }
 iv = SimpleNamespace(**image_values)
 
